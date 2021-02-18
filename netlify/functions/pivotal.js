2793e527361ff87208e21d204b216a56f3e6eccb
Nebulis <nebounet@gmail.com>
2021-02-19 03:17:01 +0800

    fix: typo in label name

--- netlify/functions/pivotal.js
+++ netlify/functions/pivotal.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 exports.handler = async function (event, context) {
   // your server-side functionality
   const response = await fetch(
-    "https://www.pivotaltracker.com/services/v5/projects/2424361/stories?with_label=open-attetation",
+    "https://www.pivotaltracker.com/services/v5/projects/2424361/stories?with_label=open-attestation",
     {
       headers: {
         "X-TrackerToken": process.env.PIVOTAL_API_TOKEN,
